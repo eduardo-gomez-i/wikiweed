a35e0623d713d02007a666ed87745ce06782fd4f
eduardo-gomez-i <gomez.i.eduardomanuel@gmail.com>
2020-05-16 19:51:38 -0500

    base

---
+++ components/hero/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hero is-medium is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          WikiWeed
+        </h1>
+        <h2 class="subtitle">
+          The Cannabis Intelligence
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    .hero {
+      background-image: url("../../static/main2.jpg");
+    }
+    h1, h2 {
+      color: #fff;
+    }
+</style>
